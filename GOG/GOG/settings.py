09c32b77b58bd5498e776d04569c643bf10ef2b3
Eolran <66948283+Eolran@users.noreply.github.com>
2024-06-12 13:53:50 +0200

    moved cors middleware at the end of the array

--- GOG/GOG/settings.py
+++ GOG/GOG/settings.py
@@ -71,7 +71,6 @@ REST_FRAMEWORK = {
 #    'DEFAULT_PERMISSION_CLASSES': ( 'rest_framework.permissions.IsAdminUser', ),
 # }
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -79,6 +78,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 
