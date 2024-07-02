c0586e8b9ab53b97bb040342e5ebcbd338ece6e5
Eolran <66948283+Eolran@users.noreply.github.com>
2024-07-02 17:37:31 +0200

    css overall start

--- front-end/src/pages/Game/Gamepage.jsx
+++ front-end/src/pages/Game/Gamepage.jsx
@@ -6,9 +6,9 @@ import fetchTsumego, { showData } from '../../services/board/solving';
 const Gamepage = () => {
     showData();
     return (
-        <div>
+        <div style={{display: 'flex', flexDirection:'column', alignItems:'center'}}>
             <h1>Game of Go</h1>
-            <GogFrame tsumego={fetchTsumego()} />
+            <GogFrame tsumego={fetchTsumego(Math.random(30))} />
         </div>
     );
 };
