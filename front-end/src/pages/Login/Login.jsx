8bc9fde826d229685a4ce3ac47a4a1257fa163c4
georgesim1 <georgesimak@gmail.com>
2024-07-07 17:16:08 +0200

    Fixed login/register routes, and adjusted css for the login and signup components

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -6,22 +6,23 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-// import { ThemeProvider } from '@mui/material/styles';
 import { useMutation } from 'react-query';
-import defaultTheme from '../../theme/defaultTheme';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/Login.css';
+import '../../styles/global.css';
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Email is required'),
+  username: Yup.string().required('Username is required'),
   password: Yup.string().required('Password is required'),
 });
 
 const SignIn = () => {
+  const navigate = useNavigate();
+
   const mutation = useMutation(async (values) => {
     const response = await fetch('http://localhost:8000/api/auth/login/', {
       method: 'POST',
@@ -40,7 +41,7 @@ const SignIn = () => {
 
   const formik = useFormik({
     initialValues: {
-      email: '',
+      username: '',
       password: '',
     },
     validationSchema: validationSchema,
@@ -48,6 +49,7 @@ const SignIn = () => {
       mutation.mutate(values, {
         onSuccess: (data) => {
           console.log('Logged in:', data);
+          navigate('/home');
         },
         onError: (error) => {
           console.error('Error:', error);
@@ -57,82 +59,80 @@ const SignIn = () => {
   });
 
   return (
-    // <ThemeProvider theme={defaultTheme}>
     <Grid
-    container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    justifyContent="center"
-    sx={{ minHeight: '100vh' }}
-  >
-    <div className="container">
-      <Container
-        component="main"
-        maxWidth="xs"
-      >
-        <CssBaseline />
-        <Box className="box">
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <form onSubmit={formik.handleSubmit} noValidate>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-              sx={{ backgroundColor: 'white' }} // Added style
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-              sx={{ backgroundColor: 'white' }} // Added style
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}
+    >
+      <div className="container">
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box className="box">
+            <Typography component="h1" variant="h5">
               Login
-            </Button>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Button href="/signup">
-                  {'Signup'}
-                </Button>
+            </Typography>
+            <form onSubmit={formik.handleSubmit} noValidate>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                error={formik.touched.username && Boolean(formik.errors.username)}
+                helperText={formik.touched.username && formik.errors.username}
+                sx={{ backgroundColor: 'white' }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+                sx={{ backgroundColor: 'white' }}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                className="login-button"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
+              <Grid container justifyContent="center">
+                <Grid item>
+                  <Button
+                    className="signup"
+                    href="/signup">
+                    {'Signup'}
+                  </Button>
+                </Grid>
               </Grid>
-            </Grid>
-          </form>
-        </Box>
-      </Container>
-    </div>
-  </Grid>
-// </ThemeProvider>
-);
+            </form>
+          </Box>
+        </Container>
+      </div>
+    </Grid>
+  );
 };
 
 export default SignIn;
