44046266259c4fc3c739dfe685d89d1f850f712c
Corentin KOZKA <65025249+Corentin-kzk@users.noreply.github.com>
2024-07-09 23:26:30 +0200

    fix(signup): fix redirect signup (#42)

--- front-end/src/services/api/auth.js
+++ front-end/src/services/api/auth.js
@@ -4,6 +4,10 @@ export const signIn = async (values) => {
    return await httpInstance.post('auth/login/', values)
 }
 
+export const signUp = async (values) => {
+   return await httpInstance.post('auth/register/', values)
+}
+
 export const signOut = async (values) => {
    return await httpInstance.post('auth/logout/')
 }
