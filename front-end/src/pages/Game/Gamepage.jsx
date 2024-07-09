79a0a66c92815e83ae745a96c91db36c83cb2086
George <57358849+georgesim1@users.noreply.github.com>
2024-07-09 20:19:09 +0200

    Design/navbar content fix (#36)
    * just removed a toolbar(no other changes)
    
    * Added navbar width to fix main content css on pages

--- front-end/src/pages/Game/Gamepage.jsx
+++ front-end/src/pages/Game/Gamepage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import GogFrame from '../../components/GogFrame/GogFrame';
 import fetchTsumego, { showData } from '../../services/board/solving';
-
+import '../../index.css';
 
 const Gamepage = () => {
     showData();
     return (
-        <div style={{display: 'flex', flexDirection:'column', alignItems:'center'}}>
+        <div className="main-content" style={{display: 'flex', flexDirection:'column', alignItems:'center'}}>
             <h1>Game of Go</h1>
             <GogFrame tsumego={fetchTsumego(Math.random(30))} />
         </div>
